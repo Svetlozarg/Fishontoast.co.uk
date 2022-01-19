6a2d5b69b1f6ed55d8c7d68e20fb52b43c3854e4
Svetlozar Georgiev <svetlozar@Svetlozars-MacBook-Pro.local>
2022-01-19 23:27:21 +0000

    FishOnToast Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router";
+import "./App.css";
+
+import Home from "./pages/Home";
+import FAQ from "./pages/FAQ";
+import Admin from "./pages/Admin";
+import Adminpanel from "./pages/Adminpanel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/faq" element={<FAQ />} />
+        <Route exact path="/fot-admin" element={<Admin />} />
+        <Route exact path="/fot-admin/adminpanel" element={<Adminpanel />} />
+      </Routes>
     </div>
   );
 }
